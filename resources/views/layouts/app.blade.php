991ed922100f54786e4766483bf9b88a8edfbf9f
Ignazio Montebello <ignaziom86@yahoo.it>
2021-07-13 11:56:18 +0200

    Ciao test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    <h3>CIAO</h3>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
