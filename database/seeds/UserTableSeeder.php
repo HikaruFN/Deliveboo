e11e4851957a20a932d41148edfc113d3e9c9d21
Francesco Nisi <francesconisi95@gmail.com>
2021-07-13 10:57:49 +0200

    seeder User bug

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            'name' => 'Gennaro Gennari',
+            'email' => 'gennarogennari@mail.com',
+            'address' => 'Via Gennaro Gennari',
+            'net_number' => 12345678901,
+            'password' => 'gennarogennaro',
+        ];
+
+        foreach($users as $user) {
+            $new_user = new User();
+            $new_user->name = $user['name'];
+            $new_user->email = $user['email'];
+            $new_user->address = $user['address'];
+            $new_user->net_number = $user['net_number'];
+            $new_user->password = $user['password'];
+            $new_user->save();
+        }
+    }
+}
