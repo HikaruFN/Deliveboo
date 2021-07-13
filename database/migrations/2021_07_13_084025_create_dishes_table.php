18e688f843271ebb00ba3084b703ee8e03a462d2
Francesco Nisi <francesconisi95@gmail.com>
2021-07-13 10:44:03 +0200

    fix migration order

--- database/migrations/2021_07_13_084025_create_dishes_table.php
+++ database/migrations/2021_07_13_084025_create_dishes_table.php
@@ -21,8 +21,8 @@ class CreateDishesTable extends Migration
             $table->boolean('visibility');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            //$table->unsignedBigInteger('category_id')->nullable();
-            //$table->foreign('category_id')->references('id')->on('categories');
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
     }
@@ -36,7 +36,7 @@ class CreateDishesTable extends Migration
     {
         Schema::table('dishes', function (Blueprint $table) {
             $table->dropForeign('dishes_user_id_foreign');
-            //$table->dropForeign('dishes_category_id_foreign');
+            $table->dropForeign('dishes_category_id_foreign');
         });
         
 
