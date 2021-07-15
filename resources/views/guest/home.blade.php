b1e4cdc6d642648f0bcd3087492ee2f3ea795a17
Francesco Nisi <francesconisi95@gmail.com>
2021-07-15 16:03:31 +0200

    address update

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -25,8 +25,7 @@
                     <img class="card-img-top" :src="restaurant.cover" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title">@{{restaurant.name}}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h4 class="card-title">@{{restaurant.address}}</h4>
                     </div>
                 </div>
            </div>
@@ -35,8 +34,7 @@
                     <img class="card-img-top" :src="restaurant.cover" alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title">@{{restaurant.name}}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h4 class="card-title">@{{restaurant.address}}</h4>
                     </div>
                 </div>
            </div>
