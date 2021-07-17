4d7173c1a53268d5d848dd0f97335dbef3300ce9
Francesco Nisi <francesconisi95@gmail.com>
2021-07-17 12:51:55 +0200

    fix home dish restaurant

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
      */
     public function show($id)
     {
-        $restaurant = Restaurant::findOrFail($id);
+        $restaurant = User::findOrFail($id);
 
         $data = [
             'restaurant' => $restaurant,
--- resources/views/guest/menu.blade.php
+++ resources/views/guest/menu.blade.php
@@ -2,20 +2,19 @@
 
 @section('content')
     <div class="container">
-        <h1>Gestisci i tuoi piatti</h1>
-
+        <h5>Piatti Ristorante</h5>
         <div class="row">
             @foreach($dishes as $dish)
                 <div class="col-6">
                     <div class="card">
-                        <div class="card-body">
-                            <h5>piatti Ristorante</h5>
-                            {{-- @if ($dish->cover)
-                                <img src="{{ asset('storage/' . $dish->cover) }}" alt="{{ $dish->name }}">
-                            @endif
-
-                            <h5 class="card-title">{{ $dish->name }}</h5>
-                             --}}
+                        <div class="card-body">  
+                            
+                                @if ($dish->cover)
+                                    {{--<img src="{{ asset('storage/' . $dish->cover) }}" alt="{{ $dish->name }}">--}}
+                                    <img src="{{$dish->cover}}" alt="{{ $dish->name }}">
+                                @endif
+                                <h5 class="card-title">{{ $dish->name }}</h5>
+                            
                         </div>
                     </div>
                 </div>
