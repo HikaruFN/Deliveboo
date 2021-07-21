f19a4f4c03fffa2a14de5c942ee7eb256c99ed40
Francesca Salvini <franci98sp@gmail.com>
2021-07-21 10:20:22 +0200

    updated css in index and show

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -4,25 +4,31 @@
     <div class="container">
         <h1 class="title">Gestisci i tuoi piatti</h1>
 
-        <div class="row">
+        <div class="row no-flex">
             @foreach($dishes as $dish)
-                <div class="col-6">
-                    <div class="card">
-                        <div class="card-body">
-                            @if ($dish->cover)
+                <div class="col-lg-8" style="margin:auto;">
+                    <div class="card margin-bottom" style="width: 100%;">
+                        <div class="card-body display-flex dish-section">
+
+                            
+                             @if ($dish->cover)
                                 <img src="{{ asset('storage/' . $dish->cover) }}" alt="{{ $dish->name }}">
                             @endif
 
-                            <h5 class="card-title">{{ $dish->name }}</h5>
-                            <a href="{{ route('admin.dishes.show', ['dish' => $dish->id]) }}" class="btn btn-primary">Vai al piatto</a>
+                            <div class="flex-items">
 
-                            <a href="{{ route('admin.dishes.edit', ['dish' => $dish->id]) }}" class="btn btn-success">Modifica piatto</a>
-                        
-                            <form action="{{ route('admin.dishes.destroy', ['dish' => $dish->id]) }}" method="post">
-                                @csrf
-                                @method ('DELETE')
-                                <input type="submit" class="btn btn-danger" value="Cancella piatto">
-                            </form>
+                                <h5 class="card-title sub-title">{{ $dish->name }}</h5>
+                                <a href="{{ route('admin.dishes.show', ['dish' => $dish->id]) }}" class="btn-coloured blue">Vai al piatto</a>
+
+                                <a href="{{ route('admin.dishes.edit', ['dish' => $dish->id]) }}" class="btn-coloured yellow">Modifica piatto</a>
+                            
+                                <form action="{{ route('admin.dishes.destroy', ['dish' => $dish->id]) }}" method="post">
+                                    @csrf
+                                    @method ('DELETE')
+                                    <input type="submit" class="btn-coloured red" value="Cancella piatto">
+                                </form>
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
