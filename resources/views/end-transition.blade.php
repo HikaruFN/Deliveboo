888cce635953304648ecc7f2e63be5438275e4cd
Francesca Salvini <franci98sp@gmail.com>
2021-07-26 11:19:22 +0200

    pagina avvenuto pagamento (css)

--- resources/views/end-transition.blade.php
+++ resources/views/end-transition.blade.php
@@ -8,14 +8,29 @@
 @section('content')
     <div id="root">
         <div class="container">
-            <h1>Acquisto avvenuto con successo</h1>
-            <p>Grazie per aver acquistatro da Deliveboo, ricerverei una Email di notifica quando il dirver partirà dal locale!. </p>
-            <h2>Riepilogo ordine:</h2>
-            <ul v-for ="element in prodottiSuccess">
-                <li> Piatto: @{{element.name}}</li>
-                <li> Quantità: @{{element.quantity}}</li>
-            </ul>
-            <h3>Totale Ordine: @{{total.toFixed(2)}}&euro;</h3>
+            <h1 class="title green-txt">Acquisto avvenuto con successo</h1>
+            
+            
+            
+            <div class="row" style="justify-content:center;">
+                <div style="text-align:center; padding:20px 0; margin-bottom:50px;" class=" col-4 "> 
+                
+                    <h4 class="black-txt bold">Riepilogo ordine</h4>
+                        <ul style="padding: 20px 0px 20px 0px;" v-for ="element in prodottiSuccess">
+                            <li>Piatto: <span class="black-txt bold">@{{element.name}}</span> </li>
+                            <li>Quantità: <span class="black-txt bold">@{{element.quantity}}</span> </li>
+                        </ul>
+                    <h4 class="red-txt bold" style="padding-bottom:30px;">Totale Ordine: @{{total.toFixed(2)}}&euro;</h4>
+    
+                    <img style="width: 130px; height:auto; margin:auto;" src="{{ asset('img/delivebo.png') }}" alt="deliveboo">
+
+                    
+    
+                </div>
+            </div>
+            
+            <p style="text-align:center;">Grazie per aver acquistato da Deliveboo, riceverai una email di notifica quando il driver partirà dal locale! </p>
+            
         </div>
     </div>
 @endsection
